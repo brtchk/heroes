fab8fda5c281066f1651c5b3af1ed39bcdf9fa92
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2018-04-21 11:53:59 +0300

    questions block

---
+++ src/components/Icons/Arrow.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Arrow = ({ color, width, handleClick, style, ...rest }) => (
+  <div onClick={handleClick} style={style}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={width} {...rest}>
+      <path fill={color} d="M20.7 5.3c1.4 0 1.9.9.9 2l-9.1 11.1c-.4.5-1.1.5-1.5 0L2.3 7.3c-.9-1.1-.5-2.1 1-2.1h17.4z" />
+    </svg>
+  </div>
+)
+
+Arrow.defaultProps = {
+  color: '#fff',
+  width: 25,
+}
+
+export default Arrow
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,33 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import injectSheet from 'react-jss'
+import { Collapse } from 'react-collapse'
 
+import Arrow from './Icons/Arrow'
 import { colors, fonts } from '../theme'
 
 const styles = {
   container: {
     fontSize: fonts.size.xs,
+    borderTop: `2px solid ${colors.brown}`,
     marginBottom: 40,
     lineHeight: 1.3,
   },
   question: {
-    color: colors.orange,
-    paddingLeft: 40,
-    maxWidth: 440,
+    color: colors.brown,
+  },
+  questionContainer: {
+    paddingTop: 10,
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   answer: {
+    paddingTop: 30,
+    maxWidth: 800,
     color: colors.brown,
   }
 }
 
-const Thesis = ({ classes, question, answer }) => (
-  <div className={classes.container}>
-    <p className={classes.question}>{question}</p>
-    <p
-      className={classes.answer}
-      dangerouslySetInnerHTML={{ __html: answer }}
-    />
-  </div>
-)
+export class Question extends Component {
+  constructor(props) {
+    super(props)
 
-export default injectSheet(styles)(Thesis)
+    this.state = {
+      isOpened: false,
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({ isOpened: !this.state.isOpened })
+  }
+
+  render() {
+    const { classes, question, answer } = this.props
+    const { isOpened } = this.state
+
+    return (
+      <div className={classes.container}>
+        <div className={classes.questionContainer}>
+          <p className={classes.question}>{question}</p>
+          <Arrow
+            width={20}
+            color={colors.orange}
+            handleClick={this.handleOpen}
+            style={{
+              cursor: 'pointer',
+              transition: 'all 0.3s ease',
+              transform: isOpened ? 'rotate(180deg)' : 'none',
+            }}
+          />
+        </div>
+        <Collapse isOpened={isOpened}>
+          <p
+            className={classes.answer}
+            dangerouslySetInnerHTML={{ __html: answer }}
+          />
+        </Collapse>
+      </div>
+    )
+  }
+}
+
+export default injectSheet(styles)(Question)
