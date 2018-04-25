7ca3356ff1cca07a82c27c11ecd68ae4127cb87f
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2018-04-25 16:18:12 +0300

    update smena-apply prices

--- src/pages/smena-5-apply.js
+++ src/pages/smena-5-apply.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 import ApplyView from '../views/ApplyView'
-import { SMENAS } from '../config/smena-apply'
+import { SMENAS } from '../config/constants'
 
 export default () => <ApplyView smena={SMENAS[4]} />
