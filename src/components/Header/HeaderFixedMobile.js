5fe79f5192ad0608ba78128f046b1379231a20d2
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2018-05-04 09:49:39 +0300

    fix call tracking while re-render part 2

--- src/components/Header/HeaderFixedMobile.js
+++ src/components/Header/HeaderFixedMobile.js
@@ -224,6 +224,19 @@ class HeaderMobile extends Component {
             }}
           />
         }
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w, d, u, i, o, s, p) {
+                  if (d.getElementById(i)) { return; } w['MangoObject'] = o;
+                  w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
+                  s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
+                  p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
+              }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
+              mgo({calltracking: {id: 12625, elements: [{"selector":".mgo-number-12625"}]}});
+            `,
+          }}
+        />
       </div>
     )
   }
