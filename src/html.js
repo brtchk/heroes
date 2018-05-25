f2533693e7e78d0820c55c789589e437e5ecbe0a
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2018-05-25 22:41:04 +0300

    update code to webvisor 2.0

--- src/html.js
+++ src/html.js
@@ -71,29 +71,29 @@ module.exports = class HTML extends React.Component {
             dangerouslySetInnerHTML={{
               __html: `
               (function (d, w, c) {
-                      (w[c] = w[c] || []).push(function() {
-                          try {
-                              w.yaCounter48533663 = new Ya.Metrika({
-                                  id:48533663,
-                                  clickmap:true,
-                                  trackLinks:true,
-                                  accurateTrackBounce:true,
-                                  webvisor:true
-                              });
-                          } catch(e) { }
-                      });
+                  (w[c] = w[c] || []).push(function() {
+                      try {
+                          w.yaCounter48533663 = new Ya.Metrika2({
+                              id:48533663,
+                              clickmap:true,
+                              trackLinks:true,
+                              accurateTrackBounce:true,
+                              webvisor:true
+                          });
+                      } catch(e) { }
+                  });
 
-                      var n = d.getElementsByTagName("script")[0],
-                          s = d.createElement("script"),
-                          f = function () { n.parentNode.insertBefore(s, n); };
-                      s.type = "text/javascript";
-                      s.async = true;
-                      s.src = "https://mc.yandex.ru/metrika/watch.js";
+                  var n = d.getElementsByTagName("script")[0],
+                      s = d.createElement("script"),
+                      f = function () { n.parentNode.insertBefore(s, n); };
+                  s.type = "text/javascript";
+                  s.async = true;
+                  s.src = "https://mc.yandex.ru/metrika/tag.js";
 
-                      if (w.opera == "[object Opera]") {
-                          d.addEventListener("DOMContentLoaded", f, false);
-                      } else { f(); }
-                  })(document, window, "yandex_metrika_callbacks");
+                  if (w.opera == "[object Opera]") {
+                      d.addEventListener("DOMContentLoaded", f, false);
+                  } else { f(); }
+              })(document, window, "yandex_metrika_callbacks2");
               `,
             }}
           />
