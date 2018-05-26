8058c1b111325795f5603923b3bf71ad61d44125
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2018-05-26 15:49:30 +0300

    metrika on top

--- src/html.js
+++ src/html.js
@@ -30,42 +30,7 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          {this.props.headComponents}
-          {css}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w, d, u, i, o, s, p) {
-                    if (d.getElementById(i)) { return; } w['MangoObject'] = o;
-                    w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
-                    s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
-                    p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
-                }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
-                mgo({calltracking: {id: 12625, elements: [{"selector":".mgo-number-12625"}]}});
-              `,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w, d, s, h, id) {
-                    w.roistatProjectId = id; w.roistatHost = h;
-                    var p = d.location.protocol == "https:" ? "https://" : "http://";
-                    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
-                    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-                })(window, document, 'script', 'cloud.roistat.com', '4667f6cfc4648b1dd776a7561a5419dc');
-              `,
-            }}
-          />
+
           <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
@@ -106,6 +71,42 @@ module.exports = class HTML extends React.Component {
             </div>
           </noscript>
 
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w, d, u, i, o, s, p) {
+                    if (d.getElementById(i)) { return; } w['MangoObject'] = o;
+                    w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
+                    s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
+                    p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
+                }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
+                mgo({calltracking: {id: 12625, elements: [{"selector":".mgo-number-12625"}]}});
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w, d, s, h, id) {
+                    w.roistatProjectId = id; w.roistatHost = h;
+                    var p = d.location.protocol == "https:" ? "https://" : "http://";
+                    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+                    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+                })(window, document, 'script', 'cloud.roistat.com', '4667f6cfc4648b1dd776a7561a5419dc');
+              `,
+            }}
+          />
           <script src="https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js" />
           <script
             type="text/javascript"
